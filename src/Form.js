deb1fee5c1b862e8a34bf6fb47ca7de121e7073d
kelly_maass <kmaass77627@gmail.com>
2024-12-29 20:51:27 -0600

    done

--- src/Form.js
+++ src/Form.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { useState } from 'react';
 
 function Form( { records } ) {
-    const [artist, setArtist] = useState("")
+    const [genre, setGenre] = useState([])
 
-    function handleArtistChange(event) {
+    function handleGenreChange(event) {
        console.log(event.target.value)
-        // setArtist(event.target.value)
+        setGenre(event.target.value)
        
     }
 
-    function handleSubmit() {
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(e)
+       let newrecords = records.filter((record) => {record.genre == genre})
 
     }
+
+
 
 return (
     <div id="form">
         <form onSubmit={handleSubmit}>
-        <input onChange={handleArtistChange} type="text" value={artist} />
+        <input onChange={handleGenreChange} type="text" value={genre} />
          <button type="submit">Submit</button>
          </form>
     </div>
--- src/RecordInfo.js
+++ src/RecordInfo.js
@@ -10,6 +10,8 @@ console.log(record)
         <div class="nothidden" id="recordinfo">
             <h1>{record.title}</h1>
             <h3>{record.artist}</h3>
+            <h4>{record.description}</h4>
+            <img id="infoimage" src={record.image} alt="record"></img>
 
         </div>
     )
