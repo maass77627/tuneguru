558ceefaff5265301af88b60b40418e5dcf661e8
kelly_maass <kmaass77627@gmail.com>
2025-02-02 15:37:10 -0600

    style

--- src/RecordInfoTwo.js
+++ src/RecordInfoTwo.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import { useState } from "react";
-
+import Track from "./Track";
 
 function RecordInfoTwo() {
 
     const [albums, setAlbums] = useState([])
+    const [tracks, setTracks] = useState([])
 
     const location = useLocation()
      const { from } = location.state
@@ -38,6 +39,7 @@ function RecordInfoTwo() {
       .then((response) => response.json())
       .then((json) => {
         console.log(json.albums.items[0].id)
+        getTracks(json.albums.items[0].id)
         setAlbums(json.albums.items)
         findAlbum(json.albums.items)
 
@@ -52,6 +54,24 @@ function RecordInfoTwo() {
     console.log(newestalbums)
    }
 
+   function getTracks(albumid){
+    console.log(albumid)
+    fetch(`https://api.spotify.com/v1/albums/${albumid}/tracks`, {
+        headers: {
+          Authorization: 'Bearer ' + from.token
+        }
+    
+      })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.items)
+        // getTracks(json.albums.items[0].id)
+        setTracks(json.items)
+        // findAlbum(json.albums.items)
+
+        })
+        console.log(tracks)
+   }
 
 
     return (
@@ -64,7 +84,14 @@ function RecordInfoTwo() {
             <img id="infoimagetwo" src={from.record.image} alt="record"></img>
             </div>
          </div>
+
+
+         <div id="trackstwo">
+         {tracks ? tracks.map((track) => <Track track={track} />) : null}
         </div>
+        </div>
+
+        
 
     )
 
