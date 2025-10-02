469725b681cddd72a1c27be179258fb2bee271cb
kelly_maass <kmaass77627@gmail.com>
2025-10-02 12:59:24 -0500

    done

--- src/RecordInfoTwo.js
+++ src/RecordInfoTwo.js
@@ -5,31 +5,23 @@ import Track from "./Track";
 
 function RecordInfoTwo() {
 
-    const [albums, setAlbums] = useState([])
+     const [albums, setAlbums] = useState([])
     const [tracks, setTracks] = useState([])
 
     const location = useLocation()
      const { from } = location.state
-    // console.log(from.record.artist)
-    // console.log(from.record.title)
-    // console.log(from.token)
     
 
-     let newartist
-    //  from.record.artist.includes(" ") ? newartist = "+" + from.record.artist.replace(/ /g, "+") : newartist = from.record.artist
+    //  let newartist
+    
 
-    let newtitle
-//    from.record.title.includes(" ") ? newtitle = from.record.title.replace(/ /g, "+") : newtitle = from.record.title
+    // let newtitle
 
-   //'https://api.spotify.com/v1/search?q=blue+album++weezer&type=album'
-  // 'https://api.spotify.com/v1/search?q=${newtitle}+${newartist}&type=album'
-  //`https://api.spotify.com/v1/search?q=${newtitle}&type=album`
      
    useEffect((newtitle, newartist) => {
     from.record.title.includes(" ") ? newtitle = from.record.title.replace(/ /g, "+") : newtitle = from.record.title
     from.record.artist.includes(" ") ? newartist = "+" + from.record.artist.replace(/ /g, "+") : newartist = from.record.artist
-    // console.log(newtitle)
-    // console.log(newartist)
+    
     fetch(`https://api.spotify.com/v1/search?q=${newtitle}${newartist}&type=album`, {
         headers: {
           Authorization: 'Bearer ' + from.token
@@ -38,24 +30,21 @@ function RecordInfoTwo() {
       })
       .then((response) => response.json())
       .then((json) => {
-        // console.log(json.albums.items[0].id)
+        
         getTracks(json.albums.items[0].id)
         setAlbums(json.albums.items)
         findAlbum(json.albums.items)
 
         })
-        // findAlbum(albums)
+        
 
    }, [])
 
    function findAlbum(albums) {
-    // console.log(albums)
     let newestalbums = albums.find((album) => album.name === from.record.title)
-    // console.log(newestalbums)
    }
 
    function getTracks(albumid){
-    // console.log(albumid)
     fetch(`https://api.spotify.com/v1/albums/${albumid}/tracks`, {
         headers: {
           Authorization: 'Bearer ' + from.token
@@ -64,13 +53,7 @@ function RecordInfoTwo() {
       })
       .then((response) => response.json())
       .then((json) => {
-        // console.log(json.items)
-        // getTracks(json.albums.items[0].id)
-        setTracks(json.items)
-        // findAlbum(json.albums.items)
-
-        })
-        // console.log(tracks)
+        setTracks(json.items)})
    }
 
 
@@ -78,24 +61,18 @@ function RecordInfoTwo() {
       <div id="background">
         <div id="tran">
         <div id="recordinfotwo">
-            {/* <div  id="recordinfotwo"> */}
             <h1 id="infotitletwo">{from.record.title}</h1>
             <h3>{from.record.artist}</h3>
             <h4>{from.record.description}</h4>
             <img id="infoimagetwo" src={from.record.image} alt="record"></img>
-            {/* </div> */}
-         </div>
-
-        <h3 id="tracktwotitle">Tracks</h3>
+           <h3 id="tracktwotitle">Tracks</h3>
          <div id="trackstwo">
          {tracks ? tracks.map((track) => <Track key={track.id} token={from.token} track={track} />) : null}
         </div>
         </div>
         </div>
-
-        
-
-    )
+        </div>
+         )
 
 }
 
